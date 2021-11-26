bd8444c9c564762a9ab33c4398a5b2aa6995d093
saralimbu2017 <iamsebi7@gmail.com>
2021-11-27 08:21:33 +1100

    Creating route for insert operation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use App\Models\Role;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create',function(){
+    $user = User::find(1);
+    $role = new Role(['name'=>'Admin']);
+    $user->roles()->save($role);
 });
